f48bbf45564d87881b443e74f3691a99574b3c78
Bondar_VR <vlad76256@gmail.com>
2022-08-30 15:58:16 +0300

    Write names for tests. Made the right architecture for the application.

--- app/app_test.go
+++ app/app_test.go
@@ -1,4 +1,4 @@
-package main
+package app
 
 import (
 	"fmt"
@@ -9,26 +9,32 @@ import (
 func TestWorkingWithAnArray(t *testing.T) {
 	//Arrange
 	testTable := []struct {
+		name     string
 		numbers  [5]int
 		expected string
 	}{
 		{
+			name:     "First",
 			numbers:  [5]int{0, 0, 0, 0, 0},
 			expected: "minimum: 0, maximum: 0",
 		},
 		{
+			name:     "Second",
 			numbers:  [5]int{1, 2, 3, 4, 5},
 			expected: "minimum: 10, maximum: 14",
 		},
 		{
+			name:     "Third",
 			numbers:  [5]int{100, 2250, 31548, 4, 5147},
 			expected: "minimum: 7501, maximum: 39045",
 		},
 		{
+			name:     "Fourth",
 			numbers:  [5]int{0, 0, 0, 0, 5},
 			expected: "minimum: 0, maximum: 5",
 		},
 		{
+			name:     "Fifth",
 			numbers:  [5]int{1, -2, 3, -4, 5},
 			expected: "minimum: -2, maximum: 7",
 		},
@@ -37,11 +43,10 @@ func TestWorkingWithAnArray(t *testing.T) {
 	//Act
 	for _, value := range testTable {
 		result := WorkingWithAnArray(value.numbers)
-
-		t.Logf("Calling arr %d, result %s", value.numbers, result)
+		t.Logf("Test name: %s; Calling arr %d, result %s", value.name, value.numbers, result)
 
 		//Assert
 		assert.Equal(t, value.expected, result,
-			fmt.Sprintf("Incorrect result. Expected %s, got %s", value.expected, result))
+			fmt.Sprintf("Incorrect result. Test name: %s; Expected %s, got %s", value.name, value.expected, result))
 	}
 }
